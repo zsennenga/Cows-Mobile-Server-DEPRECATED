299616967773b72a73e9441133dc36768dbaa33b
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-06-24 11:53:40 -0700

    Take siteid from $_GET

--- Cows-Mobile-server/CowsMobileProxy.php
+++ Cows-Mobile-server/CowsMobileProxy.php
@@ -4,7 +4,7 @@
  * 
  * Proxy used as a go-between for the mobile app and COWS.
  * 
- * Takes ticket (as a CAS TGT ID) and a whole smattering of event 
+ * Takes ticket (as a CAS TGT ID), a site id and a whole smattering of event 
  * configuration information as inputs.
  * 
  */
@@ -20,7 +20,6 @@ define("EVENT_PATH","Event/Create");
 define("LOGOUT_PATH","Account/LogOff");
 define("CAS_PROXY_PATH","https://cas.ucdavis.edu:8443/cas/proxy");
 define("COWS_BASE_PATH","http://cows.ucdavis.edu/");
-define("SITE_ID", "its");
 define("ERROR_GENERIC", "-1");
 define("ERROR_CAS", "-2");
 define("ERROR_EVENT", "-3");
@@ -41,6 +40,13 @@ function checkError($bool, $errorMessage)	{
 	}
 }
 
+if (!isset($_GET['siteid']))	{
+	exit(0);
+}
+else	{
+	define("SITE_ID",$_GET['siteid']);
+	unset($_GET['siteid']);
+}
 //Create an event object and use it to verify all our ducks are in a row regarding the request parameters.
 $event = new Event($_GET,SITE_ID);
 
